272ff5456773b55d53d3d1de33d6418d3699efed
Mickae Truman <mtruman@nyit.edu>
2019-03-25 00:10:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,9 +3,7 @@ class Person
 attr_accessor  :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def intitalize(:name, :birthday, :hair_color, :eye_color, :height,
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width)
+def intitalize(attributes)
 end
 
 end
