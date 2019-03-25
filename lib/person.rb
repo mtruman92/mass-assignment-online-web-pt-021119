4d8b9b4a24ed12f16a6cee5cc522f35a73f10ae6
Mickae Truman <mtruman@nyit.edu>
2019-03-25 00:12:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,7 @@ attr_accessor  :name, :birthday, :hair_color, :eye_color, :height, :weight, :han
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def intitalize(attributes)
+  attributes.each {|key,value| self.send(("#{key}="), value)}
 end
 
 end
